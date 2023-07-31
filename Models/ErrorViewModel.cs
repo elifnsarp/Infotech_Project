f8a9d9c23cfae2b74b63730d03abb1dfe80e3abe
elifnsarp <139148639+elifnsarp@users.noreply.github.com>
2023-08-01 02:00:29 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace fe_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
